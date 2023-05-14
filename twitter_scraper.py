f19e5183256c601aba9000fe1c29ce98d96797b5
pengqiuyuan <pengqiuyuanfj@gmail.com>
2023-05-14 11:48:15 +0800

    Update twitter_scraper.py

--- twitter_scraper.py
+++ twitter_scraper.py
@@ -9,7 +9,7 @@ DATA_PATH = pathlib.Path("data/")
 DATA_PATH.mkdir(parents=True, exist_ok=True)
 MAX_RESULT = 100
 #DATE_END = '2023-05-13'
-HASHTAG = '清华大学'
+HASHTAG = 'depression'
 JSON_FILENAME = DATA_PATH / str(datetime.datetime.today().date())
 
 def sns_scrape():
